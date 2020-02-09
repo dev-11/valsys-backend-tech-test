4635eb9e1875e246e6b9633cbabf94af94951fc1
otto <otto@masterbranch.io>
2020-02-09 23:28:50 +0000

    test update csv repo

--- Tests/Repositories/test_csv_repository.py
+++ Tests/Repositories/test_csv_repository.py
@@ -3,8 +3,7 @@ from Repositories import CsvRepository
 
 
 class CsvRepositoryTests(unittest.TestCase):
-    def test_01(self):
+    def test_get_headers_raises_StopIteration_on_empty_csv(self):
         csv_file = 'Tests/TestEnvironment/empty.csv'
         repo = CsvRepository()
-        a = repo.get_headers(csv_file)
-        self.assertTrue(True)
+        self.assertRaises(StopIteration, repo.get_headers, csv_file)
