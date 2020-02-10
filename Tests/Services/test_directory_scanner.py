96c07a7825f20978f1580f970c7d5d3b97d368ef
otto <otto@masterbranch.io>
2020-02-10 00:29:57 +0000

    test fix

--- Tests/Services/test_directory_scanner.py
+++ Tests/Services/test_directory_scanner.py
@@ -9,11 +9,9 @@ class DirectoryScannerTests(unittest.TestCase):
         ds = DirectoryScanner([dir_to_scan], get_company_registry_mock())
         result = ds.scan_new_files()
         self.assertEqual(len(result), 3)
-        self.assertEqual(result[0][0], dir_to_scan + 'empty.csv')
-        self.assertEqual(result[0][1], 'empty')
 
-        self.assertEqual(result[1][0], dir_to_scan + 'header_and_desc.csv')
-        self.assertEqual(result[1][1], 'header_and_desc')
+        expected_file_list = [[dir_to_scan + 'empty.csv', 'empty'],
+                              [dir_to_scan + 'just_header.csv', 'just_header'],
+                              [dir_to_scan + 'header_and_desc.csv', 'header_and_desc']]
 
-        self.assertEqual(result[2][0], dir_to_scan + 'just_header.csv')
-        self.assertEqual(result[2][1], 'just_header')
+        self.assertEqual(sorted(expected_file_list), sorted(result))
