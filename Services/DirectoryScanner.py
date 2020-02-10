3b879740e5b4a0724e05dc5831dcba949a6b5f9d
otto <otto@masterbranch.io>
2020-02-10 00:12:37 +0000

    file extra check

--- Services/DirectoryScanner.py
+++ Services/DirectoryScanner.py
@@ -11,7 +11,8 @@ class DirectoryScanner:
         news = []
         for current_dir in self._directories:
             for file in os.listdir(current_dir):
-                filename, extension = file.split('.')
-                if extension == config.CSV_FILE_EXTENSION and self._company_registry.is_company_unseen(filename):
-                    news.append([current_dir + file, filename])
+                if file[0] != '.':  # skipping hidden files
+                    filename, extension = file.split('.')
+                    if extension == config.CSV_FILE_EXTENSION and self._company_registry.is_company_unseen(filename):
+                        news.append([current_dir + file, filename])
         return news
